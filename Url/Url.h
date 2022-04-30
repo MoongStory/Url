9372a31d0ddb88bdeb7a58052fa7c33c002bd3e2
Moong <moongstory@gmail.com>
2022-04-30 13:28:52 +0900

    namespace 범위 변경

--- Url/Url.h
+++ Url/Url.h
@@ -1,7 +1,7 @@
 // https://github.com/MoongStory/Url
 
 #if _MSC_VER > 1000
-#pragma once
+	#pragma once
 #endif
 
 #ifndef _URL_H_
@@ -16,7 +16,7 @@ namespace MOONG
 		namespace RETURN
 		{
 			static const int SUCCESS = 0;
-
+			
 			namespace FAILURE
 			{
 				static const int PROTOCOL_NOT_FOUND = 1;
@@ -26,41 +26,41 @@ namespace MOONG
 				static const int PATH_TO_THE_FILE_NOT_FOUND = 5;
 			}
 		}
-
-		class Url
-		{
-		public:
-			// return
-			//		RETURN::SUCCESS
-			//		RETURN::FAILURE::PROTOCOL_NOT_FOUND
-			static int getProtocol(const std::string url, std::string& output);
-
-			// return
-			//		RETURN::SUCCESS
-			static int getDomainName(std::string url, std::string& output);
-
-			// return
-			//		RETURN::SUCCESS
-			//		RETURN::FAILURE::PORT_NOT_FOUND
-			static int getPort(std::string url, std::string& output);
-			static int getPort(std::string url, int* output);
-
-			// return
-			//		RETURN::SUCCESS
-			//		RETURN::FAILURE::PATH_TO_THE_FILE_NOT_FOUND
-			static int getPathToTheFile(std::string url, std::string& output);
-
-			// return
-			//		RETURN::SUCCESS
-			//		RETURN::FAILURE::PARAMETERS_NOT_FOUND
-			static int getParameters(std::string url, std::string& output);
-
-			// return
-			//		RETURN::SUCCESS
-			//		RETURN::FAILURE::ANCHOR_NOT_FOUND
-			static int getAnchor(std::string url, std::string& output);
-		};
 	}
+	
+	class Url
+	{
+	public:
+		// return
+		//		RETURN::SUCCESS
+		//		RETURN::FAILURE::PROTOCOL_NOT_FOUND
+		static int getProtocol(const std::string url, std::string& output);
+		
+		// return
+		//		RETURN::SUCCESS
+		static int getDomainName(std::string url, std::string& output);
+		
+		// return
+		//		RETURN::SUCCESS
+		//		RETURN::FAILURE::PORT_NOT_FOUND
+		static int getPort(std::string url, std::string& output);
+		static int getPort(std::string url, int* output);
+		
+		// return
+		//		RETURN::SUCCESS
+		//		RETURN::FAILURE::PATH_TO_THE_FILE_NOT_FOUND
+		static int getPathToTheFile(std::string url, std::string& output);
+		
+		// return
+		//		RETURN::SUCCESS
+		//		RETURN::FAILURE::PARAMETERS_NOT_FOUND
+		static int getParameters(std::string url, std::string& output);
+		
+		// return
+		//		RETURN::SUCCESS
+		//		RETURN::FAILURE::ANCHOR_NOT_FOUND
+		static int getAnchor(std::string url, std::string& output);
+	};
 }
 
 #endif _URL_H_
